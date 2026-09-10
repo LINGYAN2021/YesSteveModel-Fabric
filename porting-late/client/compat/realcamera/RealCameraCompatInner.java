744e19c0cb9e8b282820d5d1b3ffa56c6bb79492
LingYan2021 <cpuxiao@gmail.com>
2026-09-10 13:00:09 +0800

    删除 client/compat 联动兼容层（Fabric 26.1.2 上无对应模组，核心先行）

--- porting-late/client/compat/realcamera/RealCameraCompatInner.java
+++
@@ -1,9 +0,0 @@
-package com.elfmcys.ysm.client.compat.realcamera;
-
-import com.xtracr.realcamera.RealCameraCore;
-
-public class RealCameraCompatInner {
-    static boolean isActive() {
-        return RealCameraCore.isActive();
-    }
-}
--- porting-late/client/compat/slashblade/SlashBladeBinding.java
+++
@@ -1,9 +0,0 @@
-package com.elfmcys.ysm.client.compat.slashblade;
-
-import com.elfmcys.ysm.client.animation.molang.CtrlBinding;
-
-public class SlashBladeBinding {
-    static void addInnerBinding(CtrlBinding binding) {
-        binding.livingEntityVar("slashblade_animation", SlashBladeAnimation::getAnimationName);
-    }
-}
